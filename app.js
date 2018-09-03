afc82cf7e614574ab8d6a4c338e5fedeae5d4376
Alejandro R <Alexrom789@Gmail.com>
2018-09-03 10:22:07 -0400

    Added CORS error handling

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-const productRoutes = require('./api/routes/products')
-const orderRoutes = require('./api/routes/orders')
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+if (req.method === 'OPTIONS') {
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELTE');
+  return res.status(200).json({});
+}
+next();
+});
 
 //routes which should handle request
 app.use('/products', productRoutes);
